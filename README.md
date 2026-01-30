63b2c64e2066fb441c64dfb5c53aeb4571d5ab27
BetterCallYee <121fhqn@gmail.com>
2026-01-30 18:33:07 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project_Dev
+A group project that trains collaboration and simulates teamwork.
---
+++ pjdev/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './index.css'
+
+export default function App() {
+  return (
+    <div className=" text-3xl font-bold">
+      <h1 className='text-white'>If its working the color should be white and bg should be dark blue</h1>
+    </div>
+  )
+}
+
