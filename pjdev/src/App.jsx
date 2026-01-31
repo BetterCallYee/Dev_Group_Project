67ff8f1ceb893515c7c287034cf4a43358f89f6e
BetterCallYee <121fhqn@gmail.com>
2026-01-31 16:34:21 +0700

    Second Commit

--- pjdev/src/App.jsx
+++ pjdev/src/App.jsx
@@ -3,8 +3,9 @@ import './index.css'
 
 export default function App() {
   return (
-    <div className=" text-3xl font-bold">
-      <h1 className='text-white'>If its working the color should be white and bg should be dark blue</h1>
+    <div className=" text-3xl font-bold m-56">
+      <h1 className='text-white'>"If Its Working The Color Should Be White And Bg Should Be Dark Blue"</h1>
+      <h2 className='text-gray-500 m-4 text-center'>"Hello World"</h2>
     </div>
   )
 }
