9752455f7a041a09c61751e98c948e9d6c3c4b57
BetterCallYee <121fhqn@gmail.com>
2026-01-31 17:50:45 +0700

    Tab space

--- pjdev/src/App.jsx
+++ pjdev/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './index.css'
 
 export default function App() {
-  return (
-    <div className=" text-3xl font-bold m-56">
-      <h1 className='text-white'>"If Its Working The Color Should Be White And Bg Should Be Dark Blue"</h1>
-      <h2 className='text-gray-500 m-4 text-center'>"Hello World"</h2>
-    </div>
-  )
+    return (
+        <div className=" text-3xl font-bold m-56">
+            <h1 className='text-white'>"If Its Working The Color Should Be White And Bg Should Be Dark Blue"</h1>
+            <h2 className='text-gray-500 m-4 text-center'>"Hello World"</h2>
+        </div>
+    )
 }
-
